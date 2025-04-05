5417f53b5b20e66b1b092359d87a576e5e62b05a
Mohamad Askari <Mohamad.a117@gmail.com>
2025-04-05 10:25:48 +0330

    Initial commit: Add performance stats script

---
+++ Server_Performance_Stats.sh
@@ -0,0 +1,21 @@
+tput setaf 1; echo 'Load CPU'
+uptime
+
+echo '-----------------------------------------------------------------------------------------'
+
+tput setaf 2; echo 'Free Memory'
+free -h
+
+echo '-----------------------------------------------------------------------------------------'
+
+tput setaf 3;echo 'Free Disk'
+df -h
+
+echo '-----------------------------------------------------------------------------------------'
+
+tput setaf 4;echo 'Top 5 processes by CPU usage'
+ps -ef --sort=-pcpu | head -n 6  
+
+echo '-----------------------------------------------------------------------------------------'
+tput setaf 5; echo 'Top 5 processes by memory usage'
+ps -ef --sort -rss | head -n 6
